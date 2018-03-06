7008af6b8b5899b571ff2f85a11492b1aaaff85a
Matthias Kestenholz <mk@feinheit.ch>
2018-03-06 12:16:46 +0100

    django-http-fallback-storage 1.3.2

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def read(filename):
 
 setup(
     name='django-http-fallback-storage',
-    version='1.3.1',
+    version='1.3.2',
     description='',
     long_description=read('README.rst'),
     author='Matthias Kestenholz',
